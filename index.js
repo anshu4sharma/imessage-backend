f3b19a93d0619a729557151c58b8f045dd6ac293
anshu0x <anshusharma6327@gmail.com>
2022-10-14 01:37:37 +0530

    package json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-require("/src/db/conn");
+require("./src/db/conn");
 const router = require("./src/router/Users");
 app.get("/", (req, res) => {
   res.send("hello from anshu ");
