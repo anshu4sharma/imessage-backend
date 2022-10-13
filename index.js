6c450783b126b92988459411c72f18cd1670df00
anshu0x <anshusharma6327@gmail.com>
2022-10-14 01:53:25 +0530

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors())
 require("./src/db/conn");
 const router = require("./src/router/Users");
 app.get("/", (req, res) => {
