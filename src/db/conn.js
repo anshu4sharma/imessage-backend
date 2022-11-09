e5950e9ac2aa913a370b68414da31be4258be4b8
anshu0x <anshusharma6327@gmail.com>
2022-11-09 13:31:49 +0530

    mongodb url updated

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://anshu0x:Anshu%402008@cluster0.xgitifz.mongodb.net/login?retryWrites=true&w=majority",{
+  .connect("mongodb+srv://anshu0x:Anshu%402008@userapi.xv6o9pk.mongodb.net/?retryWrites=true&w=majority",{
   // .connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0",{
     useUnifiedTopology: true,
     useNewUrlParser: true,
