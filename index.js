f8363685b1b64d3b43a8232176b9ebc3c72654ac
anshu0x <anshusharma6327@gmail.com>
2022-11-17 14:45:06 +0530

    extra pkg removed

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 4000;
-const responseTime = require("response-time");
-var compression = require("compression");
+const PORT = process.env.PORT || 3000;
 const UserRoute = require("./src/router/Users");
 const UpiRoute = require("./src/router/UpiLink");
 var cookieParser = require("cookie-parser");
 app.use(cookieParser());
 require("./src/db/conn");
-app.use(compression());
-app.use(responseTime());
 app.use(express.json());
 app.use(
   cors({
