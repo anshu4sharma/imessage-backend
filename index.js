f66b79c29b4f6eb64e42aba68266266df808e73c
anshu0x <anshusharma6327@gmail.com>
2022-11-17 15:28:46 +0530

    https://upipayy.vercel.app/

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const UserRoute = require("./src/router/Users");
 const UpiRoute = require("./src/router/UpiLink");
 var cookieParser = require("cookie-parser");
 app.use(cookieParser());
+var session = require('express-session')
 require("./src/db/conn");
 app.use(express.json());
 app.use(
@@ -20,8 +21,13 @@ app.use(
     exposedHeaders: ["set-cookie"], // is neccessary for setting cookie
   })
 );
-app.set("trust proxy")
-
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 app.get("/", (req, res) => {
   res.send(" im anshu sharma ....");
 });
