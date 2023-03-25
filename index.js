003c028d069eab234723c3e15c8cc2f053341de3
anshusharma6327@gmail.com <anshu0x>
2023-03-25 14:55:15 +0530

    rate-limiting added for securing routes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 const UserRoute = require("./src/router/Users");
 const UpiRoute = require("./src/router/UpiLink");
-const UpiValidateRouter = require("./src/router/UpiValidator");
-var cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 require("./src/db/conn");
 app.use(express.json());
@@ -23,11 +23,23 @@ app.use(
     credentials: "true",
   })
 );
-app.get("/test", function (req, res) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000",
-  );
+const limiter4auth = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const limiter4links = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+
+app.get("/test", limiter4links,function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin",process.env.URL);
   res.setHeader("Access-Control-Allow-Credentials", true);
   console.log("Cookies: ", req.cookies);
   res.cookie("cokkieName", Math.random() * 1000, {
@@ -39,9 +51,11 @@ app.get("/test", function (req, res) {
   res.json([{ name: "anshu" }, { name: "sharma" }]);
 });
 
-app.use("/upi", UpiValidateRouter);
-app.use("/users", UserRoute);
-app.use("/genlink", UpiRoute);
+// Apply the rate limiting middleware to all requests
+
+app.use("/users", limiter4auth, UserRoute);
+app.use("/genlink",limiter4links, UpiRoute);
+
 app.listen(PORT, () => {
   console.log("Running");
 });
